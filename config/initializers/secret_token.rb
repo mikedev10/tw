a433d613d5ad027362eacea707826d3f7a2a1c36
U-squirrel\miket <miket@squirrel.(none)>
2011-06-11 18:40:10 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tw::Application.config.secret_token = 'c3731f5a7ad6b3e1dac7f22b08a73c5db8be9bb44cebc9194c6932c79599f26aa9c821b993f7b069f5a20bf0e6105c783cbe9083a396fa742ff555aa570e7b28'
